e9ecc1b8b9560f7ea74e655d742b48b7395aaf75
Gustavo Costa <costagustavom@gmail.com>
2021-11-09 14:23:46 -0300

    unit test with mockito

---
+++ modules/serviceoverride/src/main/java/test/CustomScreenNameValidatorTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.liferay.portal.kernel.util.Props;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import serviceoverride.CustomScreenNameValidator;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomScreenNameValidatorTest {
+	@Mock
+	private Props _props;
+	
+	
+	@InjectMocks
+	private CustomScreenNameValidator customScreenNameValidator;
+	
+	
+	@Test
+	public void testScreenNameWithoutDomain_ThenValidateFalse() {
+		Mockito.when(_props.get("my.property.key")).thenReturn("@mycompany.com");
+		assertFalse(customScreenNameValidator.validate(0, "asasa"));		
+	}
+
+	
+	@Test
+	public void testScreenNameWithRightDomain_ThenValidateTrue() {
+		Mockito.when(_props.get("my.property.key")).thenReturn("@mycompany.com");
+		assertTrue(customScreenNameValidator.validate(0, "asasa@mycompany.com"));
+	}
+	
+	@Test
+	public void testScreenNameWithWDomain_ThenValidateFalse() {
+		Mockito.when(_props.get("my.property.key")).thenReturn("@mycompany.com");
+		assertFalse(customScreenNameValidator.validate(0, "asasa@gmail.com"));		
+	}
+	
+	
+
+}
